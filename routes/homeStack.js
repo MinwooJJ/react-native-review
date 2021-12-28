1856a7512c3423b780d2fbee38ce8b205ec16b63
Minwoo Jung <mjung21@asu.edu>
2021-12-27 22:46:13 -0700

    Added custom header

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
+import Header from '../shared/header';
 
 const screens = {
   Home: {
     screen: Home,
-    navigationOptions: {
-      title: 'GameZone',
+    // navigation을 기능을 갖지 않은 컴포넌트로 넘겨주는 방법
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => <Header navigation={navigation} title="GameZone" />,
+      };
     },
   },
   ReviewDetails: {
