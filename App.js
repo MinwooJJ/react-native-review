461f4477a9c3ad3d9e753538a134f49e087848aa
Minwoo Jung <mjung21@asu.edu>
2021-12-27 20:45:35 -0700

    Added async loading feature

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/home';
-import About from './screens/about';
-import ReviewDetails from './screens/reviewDetails';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
+const getFonts = () =>
+  Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
+
+// Async 사용시 다음과 같이 useState를 사용하여 로딩 후 메인 컴포넌트가 동작하도록 설정 할 수 있음
 export default function App() {
-  return <Home />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
 }
 
 const styles = StyleSheet.create({});
